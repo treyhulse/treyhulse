7ae6c53267d0f899387a2bddf38370847cc5b826
treyhulse <treyhulse3@gmail.com>
2024-10-20 00:42:19 -0500

    icons

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -12,32 +12,38 @@ import {
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
+import { Home, Briefcase, FileText, Mail, Code, Brain, Wrench, ChartNoAxesCombined, Component, Sun, Building2 } from "lucide-react"
 
-const components: { title: string; href: string; description: string }[] = [
+const components: { title: string; href: string; description: string; icon: React.ReactNode }[] = [
   {
     title: "UI/UX Design",
     href: "/ui-ux",
     description: "Crafting intuitive and engaging user experiences.",
+    icon: <Component className="w-4 h-4 mr-2" />,
   },
   {
     title: "Data & Analytics",
     href: "/data-analytics",
     description: "Transforming data into actionable insights.",
+    icon: <ChartNoAxesCombined className="w-4 h-4 mr-2" />,
   },
   {
     title: "Enterprise Architecture",
     href: "/enterprise-architecture",
     description: "Building scalable and integrated IT ecosystems.",
+    icon: <Building2 className="w-4 h-4 mr-2" />,
   },
   {
     title: "Digital Transformation",
     href: "/digital-transformation",
     description: "Driving business growth through technological innovation.",
+    icon: <Brain className="w-4 h-4 mr-2" />,
   },
   {
     title: "IT Governance & Strategy",
     href: "/it-governance",
     description: "Aligning IT with business objectives and managing risks.",
+    icon: <Code className="w-4 h-4 mr-2" />,
   },
 ]
 
@@ -46,7 +52,18 @@ export function MainNav() {
     <NavigationMenu>
       <NavigationMenuList>
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Skills & Experience</NavigationMenuTrigger>
+          <Link href="/" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              <Home className="w-4 h-4 mr-2" />
+              Home
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>
+            <Wrench className="w-4 h-4 mr-2" />
+            Skills & Experience
+          </NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
               {components.map((component) => (
@@ -54,6 +71,7 @@ export function MainNav() {
                   key={component.title}
                   title={component.title}
                   href={component.href}
+                  icon={component.icon}
                 >
                   {component.description}
                 </ListItem>
@@ -64,6 +82,7 @@ export function MainNav() {
         <NavigationMenuItem>
           <Link href="/projects" legacyBehavior passHref>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              <Sun className="w-4 h-4 mr-2" />
               Projects
             </NavigationMenuLink>
           </Link>
@@ -71,6 +90,7 @@ export function MainNav() {
         <NavigationMenuItem>
           <Link href="/resume" legacyBehavior passHref>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              <FileText className="w-4 h-4 mr-2" />
               Resume
             </NavigationMenuLink>
           </Link>
@@ -78,6 +98,7 @@ export function MainNav() {
         <NavigationMenuItem>
           <Link href="/contact" legacyBehavior passHref>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              <Mail className="w-4 h-4 mr-2" />
               Contact
             </NavigationMenuLink>
           </Link>
@@ -89,8 +110,8 @@ export function MainNav() {
 
 const ListItem = React.forwardRef<
   React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
+  React.ComponentPropsWithoutRef<"a"> & { icon: React.ReactNode }
+>(({ className, title, children, icon, ...props }, ref) => {
   return (
     <li>
       <NavigationMenuLink asChild>
@@ -99,7 +120,10 @@ const ListItem = React.forwardRef<
           className={`block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground ${className}`}
           {...props}
         >
-          <div className="text-sm font-medium leading-none">{title}</div>
+          <div className="text-sm font-medium leading-none flex items-center">
+            {icon}
+            {title}
+          </div>
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
